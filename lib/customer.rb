3cbd364b481c7e9007ecfbae75cd436ce5256c9d
Ptolemy <ptolemybarnes@gmail.com>
2015-06-24 16:01:25 +0100

    Refactor and left inline tips

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,11 +1,13 @@
+require './lib/send-sms'
+
 class Customer
 
-  attr_accessor :order, :total_price, :total_quantity
+  attr_accessor :order, :total_quantity
   attr_reader :summary
 
-  def initialize
+  def initialize # consider passing in the menu to Customer, then you wouldn't have to pass it into every method that needs it
+    # @menu = menu
     @order = {}
-    @total_price = 0
     @total_quantity = 0
     # @summary = [["Item","Quantity","Price"]]
   end
@@ -15,33 +17,35 @@ class Customer
   end
 
   def add_to_order(menu,food,quantity)
-    self.order.has_key?(food.to_sym) ? self.order[food.to_sym][0] += quantity : order[food.to_sym] = [quantity, menu.items[food.to_sym]]
-    self.calculate_total_price
+    self.order.has_key?(food.to_sym) ? self.order[food.to_sym][0] += quantity : order[food.to_sym] = [quantity, menu.items[food.to_sym]] # consider refactoring out to another method that describes whats going on here
     self.calculate_total_quantity
   end
 
-  def calculate_total_price
-    self.total_price = 0
-    order.each {|k,v| self.total_price += v[0]*v[1] }
+  def total_price # in this situation we prefer a functional solution to the problem of knowing the total price. Ask Spike why!
+    total_price = 0 # try using inject to shorten this method
+    order.each {|k,v| total_price += v[0]*v[1] }
+    total_price
   end
 
   def calculate_total_quantity
-    self.total_quantity = 0
+    self.total_quantity = 0 # same as above.
     order.each {|k,v| self.total_quantity += v[0] }
   end
 
   def view_order
-    puts "Item".ljust(20) + "Quantity".center(20) + "Price".rjust(20)
+    puts "Item".ljust(20) + "Quantity".center(20) + "Price".rjust(20) # Don't use puts. Puts couples your program to STDOUT.
     order.each do |k,v|
       puts "#{k}".ljust(20) + "#{v[0]}".center(20) + "#{v[0]*v[1]}GBP".rjust(20)
     end
     puts "Total".ljust(20) + "#{total_quantity}".center(20) + "#{total_price}GBP".rjust(20)
   end
 
-  def place_order
-    fail "Total price miscalculation" if self.total_price != order.values.inject(0) {|result,element| result + element[1]}
-    load '/Users/khalidkarim/desktop/projects/takeaway/lib/send-sms.rb'
+  def place_order(text_sender)
+    fail "Total price miscalculation" unless total_price == order.values.inject(0) {|result,element| result + (element[0] * element[1])}
+    text_sender.send_message
   end
 
-
 end
+
+# There are 3 methods ending 'order', as well as an instance variable. Often this means we can break these methods out into another Class. In this case, 'Order'
+
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -8,7 +8,7 @@ describe Customer do
   end
 
   it 'should have an order that is a hash' do
-    expect(subject.order.class).to eq Hash
+    expect(subject.order.class).to eq Hash # Try to avoid type checking
   end
 
   describe 'check_menu' do
@@ -16,33 +16,33 @@ describe Customer do
   end
 
   describe 'add_to_order' do
+
+    let(:m) { double :menu, items: { burger: 10, pizza: 8, soup: 7 } }
+
+    # subject { Customer.new(m) }
+
     it 'responds to order' do
-      m = double :menu
       expect(subject).to respond_to(:add_to_order).with(3).arguments
     end
 
-    it 'should add new items to order' do
-      m = double :menu, items: {burger: 10, pizza: 8, soup: 7}
-      subject.add_to_order(m,'burger',2)
-      expect(subject.order[:burger]).to eq [2,10]
+    it 'adds new items to order' do
+      subject.add_to_order(m,'burger', 2) # this is an example of a magic number. Consider using keyword arguments.
+      expect(subject.order[:burger]).to eq [2,10] # You might want this to return a hash as it's not clear what these numbers mean
     end
 
-    it 'should update the quantity of items already in order' do
-      m = double :menu, items: {burger: 10, pizza: 8, soup: 7}
+    it 'updates the quantity of items already in order' do
       subject.add_to_order(m,'burger',2)
       subject.add_to_order(m,'burger',3)
       expect(subject.order[:burger]).to eq [5,10]
     end
 
-    it "should update the customer's total price" do
-      m = double :menu, items: {burger: 10, pizza: 8, soup: 7}
+    it "updates the customer's total price" do
       subject.add_to_order(m,'burger',2)
       subject.add_to_order(m,'pizza',3)
       expect(subject.total_price).to eq 44
     end
 
-    it "should update the customer's total quantity" do
-      m = double :menu, items: {burger: 10, pizza: 8, soup: 7}
+    it "should update the customer's total quantity" do # 'should', for me, is implied by this being a test. We're defining here what 'should' be
       subject.add_to_order(m,'burger',2)
       subject.add_to_order(m,'pizza',3)
       expect(subject.total_quantity).to eq 5
@@ -51,7 +51,7 @@ describe Customer do
 
   describe 'place order' do
     it 'should not place order if total is miscalculated' do
-      m = double :menu, items: {burger: 10, pizza: 8, soup: 7}
+      m = double :menu, items: { burger: 10, pizza: 8, soup: 7 }
       subject.add_to_order(m,'burger',2)
       subject.add_to_order(m,'pizza',3)
       subject.total_price = 0
@@ -60,11 +60,12 @@ describe Customer do
 
     it 'should send a message to customer' do
       m = double :menu, items: {burger: 10, pizza: 8, soup: 7}
+      text_sender = double :text_sender
+      expect(text_sender).to receive(:send_message).with('Your order cost 44') # Try to make this test pass.
+
       subject.add_to_order(m,'burger',2)
       subject.add_to_order(m,'pizza',3)
-    expect(subject.place_order).to raise_error "Total price miscalculation"
+      subject.place_order(text_sender)
+    end
   end
-
-
-
 end
